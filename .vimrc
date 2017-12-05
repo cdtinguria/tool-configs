0d68653fdcb6aec7e5683141f11803f8b7033be0
Chetan Tinguria <chetan.tinguria@gmail.com>
2017-12-06 00:21:14 +0530

    updates

--- .vimrc
+++ .vimrc
@@ -1,1 +1,9 @@
+syntax enable
+set background=dark
+colorscheme solarized
+
 set relativenumber
+set tabstop=2
+set laststatus=2
+set title
+set showcmd
--- copy-to-home.sh
+++ copy-to-home.sh
@@ -1,1 +1,14 @@
-cp .* ~
+#!/usr/bin/env bash
+
+cp .gitconfig ~
+
+# copy vim configs
+
+VIM_HOME_DIR=~/.vim
+
+if [ -d "$VIM_HOME_DIR" ]; then
+  cp .vimrc ~
+  cp -R colors ~/.vim
+else
+  echo ".vim not found in user_home"
+fi
